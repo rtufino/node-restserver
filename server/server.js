3b38283b22a703ac5d77f60d8e60bb4ea31ca70d
Rodrigo <rodrigo.tufio@gmail.com>
2020-01-17 12:42:58 -0500

    RESTServer - usuario

--- server/server.js
+++ server/server.js
@@ -18,12 +18,13 @@ app.use(bodyParser.json())
 
 app.use(require('./routes/usuario'));
 
-mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+    (err, res) => {
 
-    if (err) throw err;
+        if (err) throw err;
 
-    console.log('Base de datos ONLINE!');
-});
+        console.log('Base de datos ONLINE!');
+    });
 
 app.listen(process.env.PORT, () => {
     console.log("Escuchando en el puerto", process.env.PORT);
